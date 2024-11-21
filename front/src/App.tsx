310b0b34c7e3fe8be41d124c289de8362fa3a605
lecto17 <khwoo_rpsit@naver.com>
2024-11-21 09:58:52 +0900

    feat: 식당 마커 표시 및 마커 클릭 시 가게 정보 출력

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,24 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import LunchMenu from "./components/LunchMenu";
+import data from "../data/data.json";
 
 function Map() {
   const mapKey = import.meta.env.VITE_KAKAO_MAP_API_KEY;
+
+  // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+  function makeOverListener(map, marker, infowindow) {
+    return function () {
+      infowindow.open(map, marker);
+    };
+  }
+
+  // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+  function makeOutListener(infowindow) {
+    return function () {
+      infowindow.close();
+    };
+  }
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -33,17 +48,60 @@ function Map() {
           level: 3, // 지도 확대 레벨
         };
 
+        const imageSrc =
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
         // 지도 생성
         const map = new window.kakao.maps.Map(container, options);
+        let marker;
+
+        for (let i = 0; i < data.length; i++) {
+          const imageSize = new kakao.maps.Size(24, 35);
+          const latlng = new kakao.maps.LatLng(
+            data[i].latitude,
+            data[i].longitude
+          );
+
+          // 마커 이미지를 생성합니다
+          const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+          // 마커를 생성합니다
+          marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: latlng, // 마커를 표시할 위치
+            // content: data[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage, // 마커 이미지
+          });
+
+          // 마커에 표시할 인포윈도우를 생성합니다
+          const infowindow = new kakao.maps.InfoWindow({
+            content: `<div style='padding: 4px; font-size: 12px;'>${data[i].title}(${data[i].category}) 별점: ${data[i].rating}</div>`, // 인포윈도우에 표시할 내용
+          });
+
+          // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+          // 이벤트 리스너로는 클로저를 만들어 등록합니다
+          // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+          window.kakao.maps.event.addListener(
+            marker,
+            "mouseover",
+            makeOverListener(map, marker, infowindow)
+          );
+          window.kakao.maps.event.addListener(
+            marker,
+            "mouseout",
+            makeOutListener(infowindow)
+          );
+        }
 
         // 지도에 마커 추가 (옵션)
-        const markerPosition = new window.kakao.maps.LatLng(
+        const currentPosition = new window.kakao.maps.LatLng(
           latitude,
           longitude
         );
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
+        marker = new window.kakao.maps.Marker({
+          position: currentPosition,
         });
+
         marker.setMap(map);
       });
     };
