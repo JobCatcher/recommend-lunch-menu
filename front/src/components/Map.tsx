1fc14d622f4f287cdff181c4e04d96d0072ccd2d
lecto17 <khwoo_rpsit@naver.com>
2024-11-21 17:47:52 +0900

    chore: kakao 타입 추가

--- front/src/components/Map.tsx
+++ front/src/components/Map.tsx
@@ -3,19 +3,35 @@ import data from "../../data/data.json";
 import ReactDOMServer from "react-dom/server";
 import { handleClickRestaurant } from "../utils/utils";
 import Restaurant from "./Restaurant";
+import {
+  KakaoInfoWindow,
+  KakaoMap,
+  KakaoMarker,
+  KakaoNamespace,
+} from "../types/kakao";
+
+declare global {
+  interface Window {
+    kakao: KakaoNamespace;
+  }
+}
 
 const Map = () => {
   const mapKey = import.meta.env.VITE_KAKAO_MAP_API_KEY;
 
   // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-  const makeOverListener = (map, marker, infowindow) => {
+  const makeOverListener = (
+    map: KakaoMap,
+    marker: KakaoMarker,
+    infowindow: KakaoInfoWindow
+  ) => {
     return function () {
       infowindow.open(map, marker);
     };
   };
 
   // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-  const makeOutListener = (infowindow) => {
+  const makeOutListener = (infowindow: KakaoInfoWindow) => {
     return function () {
       infowindow.close();
     };
@@ -29,6 +45,7 @@ const Map = () => {
     const script = document.createElement("script");
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${mapKey}&autoload=false`;
     script.async = true;
+    const { kakao } = window;
 
     // 서울 중심 좌표
     let latitude: number = 37.5665,
@@ -41,14 +58,14 @@ const Map = () => {
 
     script.onload = () => {
       // Kakao Maps SDK 로드 후 초기화
-      window.kakao.maps.load(() => {
+      kakao.maps.load(() => {
         if (!latitude && !longitude) {
           throw new Error("위치 정보를 받아오지 못하였습니다.");
         }
 
         const container = document.getElementById("map");
         const options = {
-          center: new window.kakao.maps.LatLng(latitude, longitude),
+          center: new kakao.maps.LatLng(latitude, longitude),
           level: 3, // 지도 확대 레벨
         };
 
@@ -56,7 +73,7 @@ const Map = () => {
           "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
         // 지도 생성
-        const map = new window.kakao.maps.Map(container, options);
+        const map = new kakao.maps.Map(container, options);
         let marker;
 
         for (let i = 0; i < data.length; i++) {
@@ -88,27 +105,24 @@ const Map = () => {
           // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
           // 이벤트 리스너로는 클로저를 만들어 등록합니다
           // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-          window.kakao.maps.event.addListener(
+          kakao.maps.event.addListener(
             marker,
             "mouseover",
             makeOverListener(map, marker, infowindow)
           );
-          window.kakao.maps.event.addListener(
+          kakao.maps.event.addListener(
             marker,
             "mouseout",
             makeOutListener(infowindow)
           );
-          window.kakao.maps.event.addListener(marker, "click", () =>
+          kakao.maps.event.addListener(marker, "click", () =>
             navigateTo(data[i].title)()
           );
         }
 
         // 지도에 마커 추가 (옵션)
-        const currentPosition = new window.kakao.maps.LatLng(
-          latitude,
-          longitude
-        );
-        marker = new window.kakao.maps.Marker({
+        const currentPosition = new kakao.maps.LatLng(latitude, longitude);
+        marker = new kakao.maps.Marker({
           position: currentPosition,
         });
 
