446f1205442fdec22ac87bd00395efa13cdf8bba
lecto17 <khwoo_rpsit@naver.com>
2024-11-21 16:03:07 +0900

    design: 맛집 리스트 퍼블리싱

--- front/src/components/LunchMenu.tsx
+++ front/src/components/LunchMenu.tsx
@@ -1,7 +1,24 @@
 import { useEffect, useState } from "react";
-import data from "../../data/restaurants.json";
+// import data from "../../data/restaurants.json";
+import data from "../../data/data.json";
 import styled from "@emotion/styled";
 import { getNumbers } from "../utils/utils";
+
+// interface ReviewAndRating {
+//   별점?: string;
+//   방문자리뷰?: string;
+//   블로그리뷰?: string;
+// }
+
+// interface RestaurantInfo extends ReviewAndRating {
+//   번호: string;
+//   품목: string;
+//   상호: string;
+//   구: string;
+//   주소: string;
+//   전화번호: string;
+//   결제방법: string;
+// }
 
 interface ReviewAndRating {
   별점?: string;
@@ -10,21 +27,51 @@ interface ReviewAndRating {
 }
 
 interface RestaurantInfo extends ReviewAndRating {
-  번호: string;
-  품목: string;
-  상호: string;
-  구: string;
-  주소: string;
-  전화번호: string;
-  결제방법: string;
+  title: string;
+  latitude: number;
+  longitude: number;
+  blogReview: string;
+  countOfVisitorReview: number;
+  rating: number;
+  category: string;
+  images: string[];
 }
+
+// const LunchMenuContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   width: 100%;
+//   height: 100%;
+//   background-color: #f5f5f5;
+//   padding: 20px;
+//   box-sizing: border-box;
+// `;
+
+// const RestaurantList = styled.ul`
+//   list-style: none;
+//   padding: 0;
+//   margin: 0;
+//   width: 100%;
+//   max-width: 600px;
+// `;
+
+// const RestaurantItem = styled.li`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   justify-content: space-between;
+//   background-color: #fff;
+//   border-radius: 8px;
+// }`;
 
 interface LunchMenuProps {
   isClicked: boolean;
 }
 
 const LunchMenu = ({ isClicked }: LunchMenuProps) => {
-  const [restaurants, setResaurants] = useState<RestaurantInfo[]>([]);
+  const [restaurants, setResaurants] = useState<RestaurantInfo[]>(data);
 
   const filterRestaurantsNearSuNe = () => {
     const filteredRestaurants = data.filter((restaurant) => {
@@ -54,73 +101,130 @@ const LunchMenu = ({ isClicked }: LunchMenuProps) => {
     return await result.json();
   };
 
-  const initialize = async (restaurants: RestaurantInfo[]) => {
-    const updated = await Promise.all(
-      restaurants.map(async (restaurant) => {
-        const data: ReviewAndRating = await getRestaurantReivewAndRating(
-          restaurant.상호
-        );
-        return {
-          ...restaurant,
-          별점: getNumbers(data?.별점) || "-",
-          방문자리뷰: getNumbers(data?.방문자리뷰) || "-",
-          블로그리뷰: getNumbers(data?.블로그리뷰) || "-",
-        };
-      })
-    );
-    return updated;
-  };
-
   useEffect(() => {
-    if (isClicked) {
-      const restaurants = filterRestaurantsNearSuNe();
-      initialize(restaurants).then((updated) => {
-        setResaurants(updated);
-      });
-    }
+    // if (isClicked) {
+    //   const restaurants = filterRestaurantsNearSuNe();
+    //   initialize(restaurants).then((updated) => {
+    //     setResaurants(updated);
+    //   });
+    // }
   }, [isClicked]);
 
   return (
-    <div>
-      <h2>Lunch Menu</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>상호</th>
-            <th>위치</th>
-            <th>결제방법</th>
-            <th style={{ width: "10%" }}>별점</th>
-            <th style={{ width: "10%" }}>방문자 리뷰</th>
-            <th style={{ width: "10%" }}>블로그 리뷰</th>
-          </tr>
-        </thead>
-        <tbody>
-          {restaurants.map((restaurant) => {
-            return (
-              <StyledRow
-                key={restaurant.번호}
-                onClick={() => handleClickRestaurant(restaurant.상호)}
-              >
-                <td>{restaurant.상호}</td>
-                <td>{restaurant.주소}</td>
-                <td>{restaurant.결제방법}</td>
-                <td>{restaurant.별점}</td>
-                <td>{restaurant.방문자리뷰}</td>
-                <td>{restaurant.블로그리뷰}</td>
-              </StyledRow>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <LunchMenuContainer>
+      <h2>recommended Menu</h2>
+      <RestaurantContainer>
+        {restaurants.map((restaurant, idx) => {
+          const { title, countOfVisitorReview, rating, category, images } =
+            restaurant;
+
+          return (
+            <RestaurantWrapper
+              key={`${restaurant.title}-${idx}`}
+              onClick={() => handleClickRestaurant(title)}
+            >
+              <ImageContainer>
+                {images.map((image, idx) => {
+                  return (
+                    <img
+                      key={`${restaurant.title}-${idx}`}
+                      src={image || "https://via.placeholder.com/150"}
+                      alt={`${title} 이미지`}
+                    />
+                  );
+                })}
+              </ImageContainer>
+              <InfoContainer>
+                <Title>{title}</Title>
+                <Category>{category}</Category>
+                {/* <Description>흑돼지요리사맛집</Description> */}
+                <Review>리뷰: {countOfVisitorReview}</Review>
+                <Rating>별점: {rating}</Rating>
+              </InfoContainer>
+            </RestaurantWrapper>
+          );
+        })}
+      </RestaurantContainer>
+    </LunchMenuContainer>
   );
 };
 
 export default LunchMenu;
 
-const StyledRow = styled.tr`
-  cursor: pointer;
+const LunchMenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const RestaurantContainer = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 8px 16px;
+  width: 100%;
+  place-items: center;
+`;
+const RestaurantWrapper = styled.li`
+  width: 50%;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 16px;
+  margin-bottom: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease-in-out;
   &:hover {
-    text-decoration: underline;
+    transform: translateY(-4px);
   }
+  cursor: pointer;
+  overflow: hidden;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  margin-bottom: 12px;
+  width: 90%;
+  overflow-x: scroll;
+
+  img {
+    width: 150px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 8px;
+  }
+`;
+
+const InfoContainer = styled.div`
+  margin-left: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0 0 5px;
+`;
+
+const Category = styled.p`
+  font-size: 14px;
+  color: #777;
+  margin: 0;
+`;
+
+const Review = styled.p`
+  font-size: 14px;
+  color: #333;
+  margin: 5px 0;
+`;
+
+const Rating = styled.p`
+  font-size: 12px;
+  color: #999;
+  margin: 0;
 `;
