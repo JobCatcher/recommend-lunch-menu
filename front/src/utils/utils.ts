748dd1145f71ba3b62363d3c85331aa8044a5c42
lecto17 <khwoo_rpsit@naver.com>
2024-11-22 16:40:00 +0900

    feat: 행동구역 동 구하는 api 추가

--- front/src/utils/utils.ts
+++ front/src/utils/utils.ts
@@ -7,13 +7,36 @@ export const getNumbers = (text: unknown) => {
   return "";
 };
 
-export const handleClickRestaurant = async (storeName: string) => {
-  const name = !storeName.includes("수내") ? `수내역 ${storeName}` : storeName;
+export const handleClickRestaurant = async (
+  storeName: string,
+  dongName?: string
+) => {
+  const name = dongName ? `${dongName} ${storeName}` : `수내역 ${storeName}`;
 
-  window.open(
-    `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${name}`,
-    "_blank"
+  // window.open(
+  //   `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${name}`,
+  //   "_blank"
+  // );
+  window.open(`https://map.naver.com/v5/search/${name}`, "_blank");
+};
+
+export const getDongName = async (longitude: number, latitude: number) => {
+  const response = await fetch(
+    `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`,
+    {
+      headers: {
+        Authorization: `KakaoAK ${import.meta.env.VITE_REST_API_KEY}`,
+      },
+    }
   );
-  // window.open(`https://map.naver.com/v5/search/${name}`, "_blank");
-  // console.log("dda: ", `http://localhost:5000/api/scrape?query=${name}`);
+
+  const data = await response.json();
+
+  const { address_name, region_2depth_name, region_3depth_name } =
+    data.documents[0];
+
+  if (data?.documents?.[0].region_3depth_name.endsWith("동")) {
+    return `${region_2depth_name} ${region_3depth_name}`;
+  }
+  return address_name;
 };
