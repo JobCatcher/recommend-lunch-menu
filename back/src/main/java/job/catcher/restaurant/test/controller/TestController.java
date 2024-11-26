90b57dff452095e49dfe609191c99a72a48397f3
MoonMinHyuk1 <moon981018@gmail.com>
2024-11-26 16:03:58 +0900

    chore: cd workflow 작성

--- back/src/main/java/job/catcher/restaurant/test/controller/TestController.java
+++ back/src/main/java/job/catcher/restaurant/test/controller/TestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+
     @GetMapping("/test")
     public String test() {
         return "test";
