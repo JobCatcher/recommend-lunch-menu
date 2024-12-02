13e8770d34b928869b09a5a3595a93241a0b99fb
MoonMinHyuk1 <moon981018@gmail.com>
2024-12-02 23:35:21 +0900

    feat: 엔티티 설계 - restaurant 엔티티 설계 - thumbnail 엔티티 설계

--- back/src/main/java/job/catcher/restaurant/restaurant/domain/Restaurant.java
+++ back/src/main/java/job/catcher/restaurant/restaurant/domain/Restaurant.java
@@ -1,9 +1,14 @@
 package job.catcher.restaurant.restaurant.domain;
 
 import jakarta.persistence.*;
+import job.catcher.restaurant.thumbnail.domain.Thumbnail;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,6 +21,36 @@ public class Restaurant {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude; //위도
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude; //경도
+
+    @Column(name = "rating", nullable = false)
+    private Double rating;
+
+    @Column(name = "review_count", nullable = false)
+    private Integer reviewCount;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private Category category;
+
+    @Column(name = "thumbnails")
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Thumbnail> thumbnails = new ArrayList<>();
+
+    @Builder
+    public Restaurant(String title, Double latitude, Double longitude, Double rating, Integer reviewCount, Category category) {
+        this.title = title;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.rating = rating;
+        this.reviewCount = reviewCount;
+        this.category = category;
+    }
 }
