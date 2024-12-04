35ba4cc4bad973af3409ec0ffdac735217695c24
MoonMinHyuk1 <moon981018@gmail.com>
2024-12-04 15:55:06 +0900

    chore: Custom Response 설정 - ApiResponse 설계 - ApiException 설계

---
+++ back/src/main/java/job/catcher/restaurant/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package job.catcher.restaurant.global.handler;
+
+import job.catcher.restaurant.global.exception.ApiException;
+import job.catcher.restaurant.global.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.exception(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Object> handleApiException(ApiException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ApiResponse.exception(e.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.exception(HttpStatus.BAD_REQUEST, e.getBindingResult().getAllErrors().getFirst().getDefaultMessage()));
+    }
+}
