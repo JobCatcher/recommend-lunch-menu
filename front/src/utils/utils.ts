7cd952ec1fb9288da8be8625aa2ea6e7a32c0034
lecto17 <khwoo_rpsit@naver.com>
2024-12-14 18:06:41 +0900

    chore: 위도,경도로 거리 구하는 함수 추가

--- front/src/utils/utils.ts
+++ front/src/utils/utils.ts
@@ -69,3 +69,19 @@ export const triggerEvent = (type: 'click', object: unknown) => {
     }
   }
 };
+
+export const getDistanceFromLatLonInKm = (lat1: number, lng1: number, lat2: number, lng2: number) => {
+  //lat1:위도1, lng1:경도1, lat2:위도2, lat2:경도2
+  function deg2rad(deg: number) {
+    return deg * (Math.PI / 180);
+  }
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lng2 - lng1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+};
