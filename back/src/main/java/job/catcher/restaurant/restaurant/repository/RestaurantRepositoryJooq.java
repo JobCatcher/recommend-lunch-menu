1795389eff258f77bb4a7ce55d37fdda843ad157
MoonMinHyuk1 <moon981018@gmail.com>
2025-01-21 01:03:13 +0900

    feat: 더미데이터 설정

--- back/src/main/java/job/catcher/restaurant/restaurant/repository/RestaurantRepositoryJooq.java
+++ back/src/main/java/job/catcher/restaurant/restaurant/repository/RestaurantRepositoryJooq.java
@@ -1,6 +1,6 @@
 package job.catcher.restaurant.restaurant.repository;
 
-import job.catcher.restaurant.jobcatcherdb.enums.ThumbnailTableName;
+//import job.catcher.restaurant.jobcatcherdb.enums.ThumbnailTableName;
 import job.catcher.restaurant.restaurant.domain.Restaurant;
 import job.catcher.restaurant.restaurant.dto.RestaurantResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +8,11 @@ import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static job.catcher.restaurant.jobcatcherdb.tables.Restaurant.RESTAURANT;
-import static job.catcher.restaurant.jobcatcherdb.tables.Thumbnail.THUMBNAIL;
+//import static job.catcher.restaurant.jobcatcherdb.tables.Restaurant.RESTAURANT;
+//import static job.catcher.restaurant.jobcatcherdb.tables.Thumbnail.THUMBNAIL;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,37 +21,39 @@ public class RestaurantRepositoryJooq {
     private final DSLContext dsl;
 
     public List<Restaurant> findAllWithJooq() {
-        return dsl.selectFrom(RESTAURANT)
-                .fetchInto(Restaurant.class);
+        return new ArrayList<>();
+//        return dsl.selectFrom(RESTAURANT)
+//                .fetchInto(Restaurant.class);
     }
 
     public List<RestaurantResponseDto> findRestaurantInRangeV3(List<String> geoHashs) {
-        return dsl.select(
-                        RESTAURANT.ID,
-                        RESTAURANT.TITLE,
-                        RESTAURANT.LATITUDE,
-                        RESTAURANT.LONGITUDE,
-                        RESTAURANT.RATING,
-                        RESTAURANT.REVIEW_COUNT,
-                        DSL.multiset(
-                                DSL.select(
-                                                THUMBNAIL.ID,
-                                                THUMBNAIL.URL
-                                        )
-                                        .from(THUMBNAIL)
-                                        .where(THUMBNAIL.RECORD_ID.eq(RESTAURANT.ID)
-                                                .and(THUMBNAIL.TABLE_NAME.eq(ThumbnailTableName.RESTAURANT)))
-                        ).as("thumbnails").convertFrom(r ->
-                                r.map(thumbnailRecord ->
-                                        new RestaurantResponseDto.ThumbnailResponseDto(
-                                                thumbnailRecord.value1(), // THUMBNAIL.ID
-                                                thumbnailRecord.value2()  // THUMBNAIL.URL
-                                        )
-                                )
-                        )
-                )
-                .from(RESTAURANT)
-                .where(RESTAURANT.GEO_HASH.in(geoHashs))
-                .fetchInto(RestaurantResponseDto.class);
+        return new ArrayList<>();
+//        return dsl.select(
+//                        RESTAURANT.ID,
+//                        RESTAURANT.TITLE,
+//                        RESTAURANT.LATITUDE,
+//                        RESTAURANT.LONGITUDE,
+//                        RESTAURANT.RATING,
+//                        RESTAURANT.REVIEW_COUNT,
+//                        DSL.multiset(
+//                                DSL.select(
+//                                                THUMBNAIL.ID,
+//                                                THUMBNAIL.URL
+//                                        )
+//                                        .from(THUMBNAIL)
+//                                        .where(THUMBNAIL.RECORD_ID.eq(RESTAURANT.ID)
+//                                                .and(THUMBNAIL.TABLE_NAME.eq(ThumbnailTableName.RESTAURANT)))
+//                        ).as("thumbnails").convertFrom(r ->
+//                                r.map(thumbnailRecord ->
+//                                        new RestaurantResponseDto.ThumbnailResponseDto(
+//                                                thumbnailRecord.value1(), // THUMBNAIL.ID
+//                                                thumbnailRecord.value2()  // THUMBNAIL.URL
+//                                        )
+//                                )
+//                        )
+//                )
+//                .from(RESTAURANT)
+//                .where(RESTAURANT.GEO_HASH.in(geoHashs))
+//                .fetchInto(RestaurantResponseDto.class);
     }
 }
