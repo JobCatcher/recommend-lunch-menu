6762a12098ba7ffdadfce02dfd33138b318d3d8f
MoonMinHyuk1 <moon981018@gmail.com>
2025-02-17 14:03:43 +0900

    feat: 크롤링 후 데이터 저장 및 수정

--- back/src/main/java/job/catcher/restaurant/global/response/RestaurantCrawlingDto.java
+++ back/src/main/java/job/catcher/restaurant/global/response/RestaurantCrawlingDto.java
@@ -1,50 +1,19 @@
 package job.catcher.restaurant.global.response;
 
-import com.querydsl.core.annotations.QueryProjection;
-import job.catcher.restaurant.restaurant.domain.Restaurant;
-import job.catcher.restaurant.thumbnail.domain.Thumbnail;
 import lombok.Builder;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-//@Builder
-//public record RestaurantCrawlingDto(
-//        String title,
-//        double latitude,
-//        double longitude,
-//        Double rating,
-//        int reviewCount
-//) {
-//
-//}
 
 @Builder
 public record RestaurantCrawlingDto(
-        String statusText,
-        int status,
-        String message,
-        List<data> data
+        String googleId,
+        String title,
+        String address,
+        double latitude,
+        double longitude,
+        Double rating,
+        Integer reviewCount,
+        Integer visitedReviewCount
 ) implements Serializable {
 
-    @Builder
-    public record data(
-            long restaurantId,
-            String title,
-            double latitude,
-            double longitude,
-            Double rating,
-            int reviewCount,
-            List<ThumbnailResponseDto> thumbnails
-    ) implements Serializable {
-
-        @Builder
-        public record ThumbnailResponseDto(
-                long thumbnailId,
-                String url
-        ) implements Serializable {
-
-        }
-    }
 }
