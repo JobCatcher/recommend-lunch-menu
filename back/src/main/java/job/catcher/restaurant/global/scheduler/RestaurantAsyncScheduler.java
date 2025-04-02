97d00d81b637134c589ba56f90eee1bcfeb8386e
MoonMinHyuk1 <moon981018@gmail.com>
2025-04-03 03:10:42 +0900

    feat: 스케줄러 시간 수정

--- back/src/main/java/job/catcher/restaurant/global/scheduler/RestaurantAsyncScheduler.java
+++ back/src/main/java/job/catcher/restaurant/global/scheduler/RestaurantAsyncScheduler.java
@@ -25,7 +25,8 @@ public class RestaurantAsyncScheduler {
     private final RestaurantService restaurantService;
 
 //    @Scheduled(fixedRate = 60000)
-    @Scheduled(cron = "0 0 0 * * *")
+//    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void asyncCrawling() {
 
         Boolean lockAcquired = redisTemplate.opsForValue().setIfAbsent(LOCK_KEY, LOCK_VALUE, LOCK_EXPIRATION);
