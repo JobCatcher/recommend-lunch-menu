a229a01c5ced5f530e2593ad34402db7960a51da
lecto17 <khwoo_rpsit@naver.com>
2025-04-12 17:28:31 +0900

    chore: kakao clusterer 참조 전달하도록 수정

--- front/src/services/kakaoMap.ts
+++ front/src/services/kakaoMap.ts
@@ -1,5 +1,5 @@
 import {getDefaultStore} from 'jotai';
-import {KakaoCustomOverlay, KakaoMap, KakaoMarker, Position} from '../types/kakao';
+import {KakaoCustomOverlay, KakaoMap, KakaoMarker, KakaoMarkerClusterer, Position} from '../types/kakao';
 import {RestaurantInfo} from '../types/restaurant';
 import {getDongName, initializeMarkersOnMap, navigateToRestaurant, setActiveMarker} from '../utils/utils';
 import {customOverayAtom, markerAtom} from '../stores/mapAtom';
@@ -81,14 +81,12 @@ export const markerClickCallback = (map: KakaoMap, customOverlay: KakaoCustomOve
   };
 };
 
-export const addMarkersAndClusterer = (map: KakaoMap, restaurants: RestaurantInfo[], currentPosition: Position) => {
-  const clusterer = new window.kakao.maps.MarkerClusterer({
-    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-    minLevel: 3, // 클러스터 할 최소 지도 레벨
-    disableClickZoom: true,
-  });
-
+export const addMarkersAndClusterer = (
+  map: KakaoMap,
+  clusterer: KakaoMarkerClusterer,
+  restaurants: RestaurantInfo[],
+  currentPosition: Position,
+) => {
   const markers = initializeMarkersOnMap(map, restaurants, currentPosition);
   clusterer.addMarkers(markers.map(({marker}) => marker) as KakaoMarker[]);
 
