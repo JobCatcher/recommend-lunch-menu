cb029044c01bd3bc4dd13a1a9f57a9d31f4ed78b
lecto17 <khwoo_rpsit@naver.com>
2025-04-23 09:59:06 +0900

    chore: clusterer 관리 atom 추가

--- front/src/stores/restaurantAtom.ts
+++ front/src/stores/restaurantAtom.ts
@@ -1,6 +1,6 @@
 import {atom} from 'jotai';
-import {RestaurantInfo, RestaurantMarkersAtom} from '../types/restaurant';
-import {KakaoMarker} from '../types/kakao';
+import {clustererAtom, RestaurantInfo, RestaurantMarkersAtom} from '../types/restaurant';
+import {KakaoMarker, KakaoMarkerClusterer} from '../types/kakao';
 
 export const clickedRestaurantAtom = atom({
   activeRestaurantId: 0,
@@ -12,4 +12,8 @@ export const restaurantsAtom = atom<{restaurants: RestaurantInfo[]}>({
 
 export const restaurantMarkersAtom = atom<RestaurantMarkersAtom>({
   restaurantsMarker: new Map<number, KakaoMarker>(),
+});
+
+export const clustererMarkerAtom = atom<clustererAtom>({
+  clutererMarker: new Map<string, KakaoMarkerClusterer>(),
 });
